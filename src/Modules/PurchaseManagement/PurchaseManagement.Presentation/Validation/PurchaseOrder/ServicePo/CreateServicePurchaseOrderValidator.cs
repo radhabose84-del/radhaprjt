eae34de6253fb8333c370dcd0f49f6e9cc096a3f
Vishal <Vishal.ts@bannarimills.co.in>
2026-02-18 18:00:38 +0530

    ALL GRPCClient Missed Files Fixed.

--- src/Modules/PurchaseManagement/PurchaseManagement.Presentation/Validation/PurchaseOrder/ServicePo/CreateServicePurchaseOrderValidator.cs
+++ src/Modules/PurchaseManagement/PurchaseManagement.Presentation/Validation/PurchaseOrder/ServicePo/CreateServicePurchaseOrderValidator.cs
@@ -1,73 +1,66 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Threading.Tasks;
-// using Contracts.Interfaces.External.IWorkflow;
-// using PurchaseManagement.Application.Common.Interfaces.IPurchaseOrder.ServicePO;
-// using PurchaseManagement.Application.PurchaseOrder.Dtos.ServicePO;
-// using FluentValidation;
-// using PurchaseManagement.Presentation.Validation.Common;
-// using Shared.Validation.Common;
+using PurchaseManagement.Application.Common.Interfaces.IPurchaseOrder.ServicePO;
+using PurchaseManagement.Application.PurchaseOrder.Dtos.ServicePO;
+using FluentValidation;
+using Shared.Validation.Common;
+using Contracts.Interfaces.Lookups.Workflow;
 
-// namespace PurchaseManagement.Presentation.Validation.PurchaseOrder.ServicePo
-// {
-//     public class CreateServicePurchaseOrderValidator : AbstractValidator<CreateServicePurchaseOrderDto>
-//     {
+namespace PurchaseManagement.Presentation.Validation.PurchaseOrder.ServicePo
+{
+    public class CreateServicePurchaseOrderValidator : AbstractValidator<CreateServicePurchaseOrderDto>
+    {
 
-//         private readonly IWorkflowGrpcClient _workflowGrpcClient;
-//         private readonly IServicePurchaseOrderCommandRepository _servicePurchaseOrderCommandRepository;         
-//         private readonly List<ValidationRule> _validationRules;
+        private readonly IWorkflowLookup _workflowLookup;      
+        private readonly List<ValidationRule> _validationRules;
        
          
-//         public CreateServicePurchaseOrderValidator(IWorkflowGrpcClient workflowGrpcClient , List<ValidationRule> validationRules , IServicePurchaseOrderCommandRepository servicePurchaseOrderCommandRepository)
-//         {
-//             _workflowGrpcClient = workflowGrpcClient;
-//             _validationRules = validationRules;
-//             _servicePurchaseOrderCommandRepository = servicePurchaseOrderCommandRepository;
+        public CreateServicePurchaseOrderValidator(IWorkflowLookup workflowLookup , List<ValidationRule> validationRules , IServicePurchaseOrderCommandRepository servicePurchaseOrderCommandRepository)
+        {
+            _workflowLookup = workflowLookup;
+            _validationRules = validationRules;
 
-//             if (_validationRules == null || !_validationRules.Any())
-//             {
-//                 throw new InvalidOperationException("Validation rules could not be loaded.");
-//             }
-//              foreach (var rule in _validationRules)
-//             {
-//                 switch (rule.Rule)
-//                 {
+            if (_validationRules == null || !_validationRules.Any())
+            {
+                throw new InvalidOperationException("Validation rules could not be loaded.");
+            }
+             foreach (var rule in _validationRules)
+            {
+                switch (rule.Rule)
+                {
 
-//                    case "Workflow":
-//                                     RuleFor(x => x.UnitId)
-//                                 .MustAsync(async (unitId, cancellation) =>
-//                                     await _workflowGrpcClient.IsApproveWorkflowConfigure(
-//                                         PurchaseManagement.Domain.Common.MiscEnumEntity.ServicePO, // entity type
-//                                         unitId,
-//                                         0))                      // DepartmentId not required, pass null
-//                                 .WithMessage(rule.Error);
-//                             break;
-//                 }
-//             }
+                   case "Workflow":
+                                    RuleFor(x => x.UnitId)
+                                .MustAsync(async (unitId, cancellation) =>
+                                    await _workflowLookup.IsApproveWorkflowConfigureAsync(
+                                        PurchaseManagement.Domain.Common.MiscEnumEntity.ServicePO, // entity type
+                                        unitId,
+                                        0))                      // DepartmentId not required, pass null
+                                .WithMessage(rule.Error);
+                            break;
+                }
+            }
 
-//             RuleFor(x => x.UnitId).GreaterThan(0);
-//             RuleFor(x => x.PODate).NotEmpty();
-//             RuleFor(x => x.VendorId).GreaterThan(0);
-//             RuleForEach(x => x.ServicePos).SetValidator(new ServiceHeaderValidator());
-//         }
+            RuleFor(x => x.UnitId).GreaterThan(0);
+            RuleFor(x => x.PODate).NotEmpty();
+            RuleFor(x => x.VendorId).GreaterThan(0);
+            RuleForEach(x => x.ServicePos).SetValidator(new ServiceHeaderValidator());
+        }
 
-//         private class ServiceHeaderValidator : AbstractValidator<PurchaseOrderServiceHeaderDto>
-//         {
-//             public ServiceHeaderValidator()
-//             {
-//                 RuleFor(x => x.ServiceCategoryId).GreaterThan(0);
-//                 RuleForEach(x => x.Lines).SetValidator(new ServiceLineValidator());
-//             }
-//         }
+        private class ServiceHeaderValidator : AbstractValidator<PurchaseOrderServiceHeaderDto>
+        {
+            public ServiceHeaderValidator()
+            {
+                RuleFor(x => x.ServiceCategoryId).GreaterThan(0);
+                RuleForEach(x => x.Lines).SetValidator(new ServiceLineValidator());
+            }
+        }
 
-//         private class ServiceLineValidator : AbstractValidator<PurchaseOrderServiceLineDto>
-//         {
-//             public ServiceLineValidator()
-//             {
-//                 RuleFor(x => x.PlannedQuantity).GreaterThan(0);
-//                 RuleFor(x => x.PlannedRate).GreaterThan(0);
-//             }
-//         }
-//     }
-// }
+        private class ServiceLineValidator : AbstractValidator<PurchaseOrderServiceLineDto>
+        {
+            public ServiceLineValidator()
+            {
+                RuleFor(x => x.PlannedQuantity).GreaterThan(0);
+                RuleFor(x => x.PlannedRate).GreaterThan(0);
+            }
+        }
+    }
+}
