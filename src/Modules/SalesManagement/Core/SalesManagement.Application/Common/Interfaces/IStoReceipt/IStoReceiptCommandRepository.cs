cefc427955cc2d1ef30364ac53dd34ee85e67623
Dhayal <dhayal@bannarimills.co.in>
2026-03-11 12:50:24 +0530

    DOCUMENT  NUMBER  BASED  MASTER  TABLE  FOR  STO, STODC AND  STORECEIPT

--- src/Modules/SalesManagement/Core/SalesManagement.Application/Common/Interfaces/IStoReceipt/IStoReceiptCommandRepository.cs
+++ src/Modules/SalesManagement/Core/SalesManagement.Application/Common/Interfaces/IStoReceipt/IStoReceiptCommandRepository.cs
@@ -2,7 +2,6 @@ namespace SalesManagement.Application.Common.Interfaces.IStoReceipt
 {
     public interface IStoReceiptCommandRepository
     {
-        Task<string> GenerateNextStoReceiptNumberAsync(int receivingPlantId, CancellationToken ct = default);
-        Task<int> CreateAsync(Domain.Entities.StoReceiptHeader entity, int packedStatusId);
+        Task<int> CreateAsync(Domain.Entities.StoReceiptHeader entity, int packedStatusId, int damagedStatusId, int typeId);
     }
 }
