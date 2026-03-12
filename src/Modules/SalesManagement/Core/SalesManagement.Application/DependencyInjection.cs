241ffe2eb7fae217f106edf5351196dbc8c47b5c
BANNARI <itsupport@bannarimills.co.in>
2026-03-12 13:30:22 +0530

    DI commented

--- src/Modules/SalesManagement/Core/SalesManagement.Application/DependencyInjection.cs
+++ src/Modules/SalesManagement/Core/SalesManagement.Application/DependencyInjection.cs
@@ -1,26 +1,26 @@
-using System.Reflection;
-using Microsoft.Extensions.DependencyInjection;
+// using System.Reflection;
+// using Microsoft.Extensions.DependencyInjection;
 
 
-namespace SalesManagement.Application
-{
-    public static class DependencyInjection
-    {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
-        {
-            if (services == null) throw new ArgumentNullException(nameof(services));
+// namespace SalesManagement.Application
+// {
+//     public static class DependencyInjection
+//     {
+//         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+//         {
+//             if (services == null) throw new ArgumentNullException(nameof(services));
 
-            // Use a specific AddAutoMapper overload
-            services.AddAutoMapper(cfg => cfg.AddMaps(Assembly.GetExecutingAssembly()));
+//             // Use a specific AddAutoMapper overload
+//             services.AddAutoMapper(cfg => cfg.AddMaps(Assembly.GetExecutingAssembly()));
 
 
-            // Add MediatR
-            services.AddMediatR(cfg =>
-            {
-                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-            });
+//             // Add MediatR
+//             services.AddMediatR(cfg =>
+//             {
+//                 cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+//             });
 
-            return services;
-        }
-    }
-}
+//             return services;
+//         }
+//     }
+// }
