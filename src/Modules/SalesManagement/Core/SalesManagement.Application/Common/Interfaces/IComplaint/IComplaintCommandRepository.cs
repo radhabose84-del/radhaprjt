1d5051b72d1daaee19b051f8c17006a01c62e1cc
Dhayal <dhayal@bannarimills.co.in>
2026-03-26 12:50:03 +0530

    document number  added

--- src/Modules/SalesManagement/Core/SalesManagement.Application/Common/Interfaces/IComplaint/IComplaintCommandRepository.cs
+++ src/Modules/SalesManagement/Core/SalesManagement.Application/Common/Interfaces/IComplaint/IComplaintCommandRepository.cs
@@ -4,7 +4,7 @@ namespace SalesManagement.Application.Common.Interfaces.IComplaint
 {
     public interface IComplaintCommandRepository
     {
-        Task<int> CreateAsync(ComplaintHeader entity);
+        Task<int> CreateAsync(ComplaintHeader entity, int typeId);
         Task<int> UpdateAsync(ComplaintHeader entity, List<ComplaintDetail> details);
         Task<bool> SoftDeleteAsync(int id, CancellationToken ct);
         Task UpdateApprovalStatusAsync(int id, string status, CancellationToken ct);
