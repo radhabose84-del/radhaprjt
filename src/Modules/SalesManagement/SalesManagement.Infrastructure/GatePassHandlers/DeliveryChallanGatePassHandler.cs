78b6a5d5d48a806ca364e7489415a466931e76e1
praveen <praveen@bannarimills.co.in>
2026-03-27 17:33:08 +0530

    gate Pass Cancel option Provided

--- src/Modules/SalesManagement/SalesManagement.Infrastructure/GatePassHandlers/DeliveryChallanGatePassHandler.cs
+++ src/Modules/SalesManagement/SalesManagement.Infrastructure/GatePassHandlers/DeliveryChallanGatePassHandler.cs
@@ -14,5 +14,11 @@ namespace SalesManagement.Infrastructure.GatePassHandlers
             const string sql = "UPDATE [Sales].[DeliveryChallanHeader] SET GEFlag = 1 WHERE Id = @Id AND IsDeleted = 0";
             await connection.ExecuteAsync(sql, new { Id = documentId }, transaction);
         }
+
+        public async Task RevertGatePassAsync(int documentId, DbConnection connection, DbTransaction transaction)
+        {
+            const string sql = "UPDATE [Sales].[DeliveryChallanHeader] SET GEFlag = 0 WHERE Id = @Id AND IsDeleted = 0";
+            await connection.ExecuteAsync(sql, new { Id = documentId }, transaction);
+        }
     }
 }
