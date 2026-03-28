ebe6176f43b1cc3039b146890e4e93f9422ffdd0
praveen <praveen@bannarimills.co.in>
2026-03-28 14:52:26 +0530

    Gatepass Changes DocId added

--- src/Modules/GateEntryManagement/Core/GateEntryManagement.Application/GatePass/Dto/CreateGatePassDetailDto.cs
+++ src/Modules/GateEntryManagement/Core/GateEntryManagement.Application/GatePass/Dto/CreateGatePassDetailDto.cs
@@ -3,6 +3,7 @@ namespace GateEntryManagement.Application.GatePass.Dto
     public class CreateGatePassDetailDto
     {
         public int DocTypeId { get; set; }
+        public int DocId { get; set; }
         public string? DocNo { get; set; }
         public string? PartyName { get; set; }
         public string? PartyCode { get; set; }
