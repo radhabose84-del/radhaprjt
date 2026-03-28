7c1d1d61da3855282b75608ed716ef3a236c058e
Vishal <Vishal.ts@bannarimills.co.in>
2026-03-28 13:03:46 +0530

    Yarn Twist Master

--- src/Modules/ProductionManagement/ProductionManagement.Infrastructure/Migrations/20260328070628_YarnTwistMasterTable.Designer.cs
+++ src/Modules/ProductionManagement/ProductionManagement.Infrastructure/Migrations/20260328070628_YarnTwistMasterTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductionManagement.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using ProductionManagement.Infrastructure.Data;
 namespace ProductionManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260328070628_YarnTwistMasterTable")]
+    partial class YarnTwistMasterTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
