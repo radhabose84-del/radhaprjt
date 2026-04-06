a2a77175482a2bb71f0ecbdac6edfd1e3a0063f5
praveen <praveen@bannarimills.co.in>
2026-04-06 18:16:44 +0530

    table migartion

--- src/Modules/LogisticsManagement/LogisticsManagement.Infrastructure/Migrations/20260406124558_FreightMaster.Designer.cs
+++ src/Modules/LogisticsManagement/LogisticsManagement.Infrastructure/Migrations/20260406124558_FreightMaster.Designer.cs
@@ -4,6 +4,7 @@ using LogisticsManagement.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LogisticsManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260406124558_FreightMaster")]
+    partial class FreightMaster
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
