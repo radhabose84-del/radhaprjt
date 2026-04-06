c737749d6fbfc4af542d20af9592a9cfa15bc40f
praveen <praveen@bannarimills.co.in>
2026-04-06 15:53:41 +0530

    Revert migartion

---
+++ src/Modules/SalesManagement/SalesManagement.Infrastructure/Migrations/20260406102220_RemoveFrightIdDispatchAddressMaster.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesManagement.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveFrightIdDispatchAddressMaster : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DispatchAddressMaster_FreightMaster_FreightId",
+                schema: "Sales",
+                table: "DispatchAddressMaster");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DispatchAddressMaster_FreightId",
+                schema: "Sales",
+                table: "DispatchAddressMaster");
+
+            migrationBuilder.DropColumn(
+                name: "FreightId",
+                schema: "Sales",
+                table: "DispatchAddressMaster");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FreightId",
+                schema: "Sales",
+                table: "DispatchAddressMaster",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DispatchAddressMaster_FreightId",
+                schema: "Sales",
+                table: "DispatchAddressMaster",
+                column: "FreightId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DispatchAddressMaster_FreightMaster_FreightId",
+                schema: "Sales",
+                table: "DispatchAddressMaster",
+                column: "FreightId",
+                principalSchema: "Sales",
+                principalTable: "FreightMaster",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
