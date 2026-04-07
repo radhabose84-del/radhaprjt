51cdb42a11a553e4267c859a12ba4bd80efadc36
praveen <praveen@bannarimills.co.in>
2026-04-07 12:11:43 +0530

    Frieght Master Changes and LookUp Added

--- src/Modules/LogisticsManagement/Core/LogisticsManagement.Application/FreightMaster/Queries/GetFreightMasterAutoComplete/GetFreightMasterAutoCompleteQuery.cs
+++ src/Modules/LogisticsManagement/Core/LogisticsManagement.Application/FreightMaster/Queries/GetFreightMasterAutoComplete/GetFreightMasterAutoCompleteQuery.cs
@@ -3,6 +3,6 @@ using LogisticsManagement.Application.FreightMaster.Dto;
 
 namespace LogisticsManagement.Application.FreightMaster.Queries.GetFreightMasterAutoComplete
 {
-    public sealed record GetFreightMasterAutoCompleteQuery(string Term)
+    public sealed record GetFreightMasterAutoCompleteQuery(string Term, int? ModuleId = null)
         : IRequest<IReadOnlyList<FreightMasterLookupDto>>;
 }
