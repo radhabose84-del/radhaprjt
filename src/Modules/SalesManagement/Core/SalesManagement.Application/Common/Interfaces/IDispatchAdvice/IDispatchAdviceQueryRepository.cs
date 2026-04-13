5b567f54e089a342bef4b9f0785b6a6b5f2e3ad3
praveen <praveen@bannarimills.co.in>
2026-04-13 10:17:28 +0530

    Disptach Advice Packing List Report Changes

--- src/Modules/SalesManagement/Core/SalesManagement.Application/Common/Interfaces/IDispatchAdvice/IDispatchAdviceQueryRepository.cs
+++ src/Modules/SalesManagement/Core/SalesManagement.Application/Common/Interfaces/IDispatchAdvice/IDispatchAdviceQueryRepository.cs
@@ -13,6 +13,7 @@ namespace SalesManagement.Application.Common.Interfaces.IDispatchAdvice
         Task<List<DispatchAdviceStockDto>> GetStockAsync(int itemId, int lotId, int statusId);
         Task<List<int>> GetAvailablePackNosAsync(int itemId, int lotId, int statusId, int startPackNo, int endPackNo, int packTypeId);
         Task<List<DispatchAdvicePackRangeDto>> GetPackRangeAsync(int itemId, int lotId, int packTypeId, int statusId, int range, string? orderType);
+        Task<DispatchAdvicePackingListDto?> GetPackingListAsync(int dispatchAdviceId, CancellationToken ct);
         Task<bool> NotFoundAsync(int id);
         Task<bool> HasInvoiceAsync(int dispatchAdviceId);
         Task<IReadOnlyList<DispatchAdviceLookupDto>> AutocompleteAsync(string term, CancellationToken ct);
