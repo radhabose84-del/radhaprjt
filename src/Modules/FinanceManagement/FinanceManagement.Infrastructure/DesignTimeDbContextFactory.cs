1a7f8fc84ed015dc04ddffc4830aa415dd26748c
Vishal <Vishal.ts@bannarimills.co.in>
2026-04-20 11:17:39 +0530

    Token has been added with unittype and unittype id

--- src/Modules/FinanceManagement/FinanceManagement.Infrastructure/DesignTimeDbContextFactory.cs
+++ src/Modules/FinanceManagement/FinanceManagement.Infrastructure/DesignTimeDbContextFactory.cs
@@ -64,6 +64,8 @@ namespace FinanceManagement.Infrastructure
             public string GetGroupCode() => string.Empty;
             public int GetEntityId() => 0;
             public int? GetUnitId() => null;
+            public int GetUnitTypeId() => 0;
+            public string GetUnitTypeName() => string.Empty;
             public string GetOldUnitId() => string.Empty;
             public int? GetPartyId() => null;
             public int? GetEmpId() => null;
