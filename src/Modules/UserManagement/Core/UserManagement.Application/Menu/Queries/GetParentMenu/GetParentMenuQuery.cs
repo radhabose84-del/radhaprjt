63d0848e5b20795a0954817206464c1a9ba40108
Dhayal <dhayal@bannarimills.co.in>
2026-04-21 12:43:48 +0530

    menu control based  on moduleid

--- src/Modules/UserManagement/Core/UserManagement.Application/Menu/Queries/GetParentMenu/GetParentMenuQuery.cs
+++ src/Modules/UserManagement/Core/UserManagement.Application/Menu/Queries/GetParentMenu/GetParentMenuQuery.cs
@@ -5,5 +5,6 @@ namespace UserManagement.Application.Menu.Queries.GetParentMenu
     public class GetParentMenuQuery : IRequest<List<ParentMenuDto>>
     {
         public string? SearchPattern { get; set; }
+        public int? ModuleId { get; set; }
     }
 }
