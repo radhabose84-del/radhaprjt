b2f34e3ad3a6a91ab887c03c65009551dabb6a25
Dhayal <dhayal@bannarimills.co.in>
2026-04-28 18:20:34 +0530

    returnlocation id from warehouse with migration

---
+++ src/Modules/SalesManagement/SalesManagement.Infrastructure/Migrations/20260428123848_ComplaintResolution_ReturnLocationToWarehouse.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesManagement.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ComplaintResolution_ReturnLocationToWarehouse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ComplaintResolution_MiscMaster_ReturnLocationId",
+                schema: "Sales",
+                table: "ComplaintResolution");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ComplaintResolution_ReturnLocationId",
+                schema: "Sales",
+                table: "ComplaintResolution");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ComplaintResolution_ReturnLocationId",
+                schema: "Sales",
+                table: "ComplaintResolution",
+                column: "ReturnLocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ComplaintResolution_MiscMaster_ReturnLocationId",
+                schema: "Sales",
+                table: "ComplaintResolution",
+                column: "ReturnLocationId",
+                principalSchema: "Sales",
+                principalTable: "MiscMaster",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
