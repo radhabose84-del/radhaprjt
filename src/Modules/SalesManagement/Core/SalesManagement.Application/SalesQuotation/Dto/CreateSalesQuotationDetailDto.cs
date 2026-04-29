f30e76656d3fd84ee219c98295b76ffcab9cf7da
Vishal <Vishal.ts@bannarimills.co.in>
2026-04-29 16:53:46 +0530

    Sales Qouetation added with columns of Varient  Id , UOM and discount type id with miscs

--- src/Modules/SalesManagement/Core/SalesManagement.Application/SalesQuotation/Dto/CreateSalesQuotationDetailDto.cs
+++ src/Modules/SalesManagement/Core/SalesManagement.Application/SalesQuotation/Dto/CreateSalesQuotationDetailDto.cs
@@ -3,9 +3,12 @@ namespace SalesManagement.Application.SalesQuotation.Dto
     public class CreateSalesQuotationDetailDto
     {
         public int ItemId { get; set; }
+        public int? VariantId { get; set; }
         public decimal Quantity { get; set; }
+        public int? UOMId { get; set; }
         public decimal ExMillRate { get; set; }
         public decimal Discount { get; set; }
+        public int? DiscountTypeId { get; set; }
         public decimal NetRate { get; set; }
         public decimal TotalAmount { get; set; }
         public int HSNId { get; set; }
