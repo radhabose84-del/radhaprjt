fbe382595bb7be4cf31cd8a362509c26a086d2e1
praveen <praveen@bannarimills.co.in>
2026-06-01 13:35:46 +0530

    gate changes

---
+++ src/Modules/GateEntryManagement/GateEntryManagement.Infrastructure/Migrations/20260601080503_GateInwardDtl_ExpiryDate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GateEntryManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class GateInwardDtl_ExpiryDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ExpiryDate",
+                schema: "Gate",
+                table: "GateInwardDtl",
+                type: "datetimeoffset",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpiryDate",
+                schema: "Gate",
+                table: "GateInwardDtl");
+        }
+    }
+}
